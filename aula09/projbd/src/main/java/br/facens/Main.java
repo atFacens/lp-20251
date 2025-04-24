9e342ef9425fc099b40c752532c6ad572e34d705
Emerson Paduan <emerson@paduan.dev.br>
2025-04-23 22:43:44 -0300

    aula 10

--- aula09/projbd/src/main/java/br/facens/Main.java
+++ aula09/projbd/src/main/java/br/facens/Main.java
@@ -1,10 +1,24 @@
 package br.facens;
 
 import br.facens.dao.ClienteDAO;
+import br.facens.model.Cliente;
 
 public class Main {
     public static void main(String[] args) {
-        ClienteDAO.conectBD();
+        // ClienteDAO.conectBD();
+
         
+        // ClienteDAO.criarTabelaCliente();
+        
+        
+        Cliente c = new Cliente(3, "Amanda", "amanda@facens.br");
+
+        boolean inserido = ClienteDAO.inserir(c);
+
+        if(inserido) {
+            System.out.println("Cliente inserido com sucesso");
+        } else {
+            System.out.println("Erro na inserção");
+        }
     }
 }
--- aula09/projbd/src/main/java/br/facens/dao/ClienteDAO.java
+++ aula09/projbd/src/main/java/br/facens/dao/ClienteDAO.java
@@ -2,6 +2,11 @@ package br.facens.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import br.facens.model.Cliente;
 
 public class ClienteDAO {
     private static final String url = "jdbc:mysql://localhost:3306/aulalp_facens";
@@ -23,4 +28,53 @@ public class ClienteDAO {
             System.out.println("Erro: " + e.getMessage());
         }
     }
+
+    public static void criarTabelaCliente() {
+        Connection connection;
+
+        try {
+            System.out.println("Conectando ao BD...");
+            connection = DriverManager.getConnection(url, username, password);
+
+            String sql = "create table cliente (id int, nome varchar(150), email varchar(100));";
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            System.out.println("Tabela criada");
+
+            statement.close();
+            connection.close();
+            System.out.println("Conexão terminada");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static boolean inserir(Cliente cliente) {
+        Connection connection;
+
+        try {
+            System.out.println("Conectando ao BD...");
+            connection = DriverManager.getConnection(url, username, password);
+
+            String sql = "insert into cliente values(?,?,?)";
+            
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, cliente.getId());
+            preparedStatement.setString(2, cliente.getName());
+            preparedStatement.setString(3, cliente.getEmail());
+
+            int result = preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+
+            return (result > 0);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
 }
+
